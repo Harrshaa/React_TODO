7bb6d29bcb812a24e0576733f6afa3e300a4c82d
Harrshaa <sriharsha.mutakodoor@gmail.com>
2023-10-03 13:53:31 +0530

    delete n add task

--- App.js
+++ App.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import { useState } from 'react'
+import "./App.css"
+import Form from './src/components/Form'
+import TodoList from './src/components/TodoList'
 
 const App = () => {
+  const [inputText, setInputText] = useState("");
+  const [todos,setTodos] =useState([]);
+
   return (
-    <div>Appu</div>
+    <div>
+        <header>Todo List </header>      
+        
+        <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText} />
+        <TodoList todos={todos} setTodos={setTodos} />
+
+    </div>
   )
 }
 
---
+++ src/components/TodoList.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Todo from './Todo';
+
+const TodoList = ({todos,setTodos}) => {
+    console.log(todos);
+  return (
+    <div className='todo-container'>
+        <ul className='todo-list'>
+          {todos.map((todo) => (
+            <Todo 
+            text={todo.text}
+            key={todo.id}
+            setTodos={setTodos}
+            todos={todos} 
+            todo={todo}
+            />
+          ))}
+        </ul>
+    </div>
+  )
+}
+
+export default TodoList;
